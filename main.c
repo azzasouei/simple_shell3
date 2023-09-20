50be34693aba178067d481e6e659dea4e461c734
azzasouei <azzasouei@gmail.com>
2023-09-20 14:07:52 -0700

    project shell

---
+++ main.c
@@ -0,0 +1,32 @@
+#include "hsh.h"
+int terminal_mode(info_t *info);
+int is_delim(char c, char *delim);
+int isalpha_(int c);
+int _atoi(char *s);
+char *begins_with(const char *str, const char *substr);
+char *strcat_(char *dest, char *src);
+char *strcpy_(char *dest, char *src);
+char *strdup_(const char *str);
+char *strchr_(char *str, char c);
+char **str_to_vec(char *str);
+char *memset_(char *dest, char val, unsigned int n);
+void arr_free(char **arr);
+void *realloc_(void *ptr, unsigned int old_size, unsigned int new_size);
+int bfree(void **ptr);
+
+int main(void)
+{
+	info_t info;
+	memset(&info, 0, sizeof(info_t));
+
+	if (terminal_mode(&info))
+	{
+		printf("Interactive mode\n");
+	}
+	else
+	{
+		printf("Not in interactive mode\n");
+	}
+
+	return (EXIT_SUCCESS);
+}
