0b86e7d5525a435882c161ddf88838dbd3f25e25
azzasouei <azzasouei@gmail.com>
2023-09-21 05:15:16 -0700

    prompt

---
+++ prompt.c
@@ -0,0 +1,43 @@
+#include "hsh.h"
+
+#define MAX_INPUT_LENGTH 1024
+/**
+ * main - Simple UNIX-like shell
+ * Return: 0
+ */
+int main(void)
+{
+	char input[MAX_INPUT_LENGTH];
+
+	for (;;)
+	{
+		printf("simple_shell> ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
+		{
+			printf("\nExiting simple_shell.\n");
+			break;
+		}
+		input[strcspn(input, "\n")] = '\0';
+
+		pid_t pid = fork();
+
+		if (pid == -1)
+		{
+			perror("Fork error");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0)
+		{
+			if (execlp(input, input, NULL) == -1)
+			{
+				perror("Exec error");
+				exit(EXIT_FAILURE);
+			}
+		} else
+		{
+			int status;
+
+			wait(&status);
+		}
+	}
+	return (0);
+}
