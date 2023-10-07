0fe22a8198a932598a2d2ee739165b1e6281ccd2
Yosrahabouria <yosrahabouria@gmail.com>
2023-10-07 10:07:09 -0700

    a

--- environment.c
+++ environment.c
@@ -1,23 +1,23 @@
 #include "hsh.h"
 
 /**
- * printEnvironment - Print the current environment.
+ * print_environment - Print the current environment.
  * @info: Structure containing potential arguments.
  * Return: Always 0.
  */
-int printEnvironment(info_t *info)
+int print_environment(info_t *info)
 {
-print_list_str(info->env);
+    print_list_str(info->env);
     return 0;
 }
 
 /**
- * getEnvironmentValue - Get the value of an environment variable.
+ * get_environment_value - Get the value of an environment variable.
  * @info: Structure containing potential arguments.
  * @name: Env variable name.
  * Return: The value or NULL if not found.
  */
-char *getEnvironmentValue(info_t *info, const char *name)
+char *get_environment_value(info_t *info, const char *name)
 {
     for (list_t *node = info->env; node; node = node->next)
     {
@@ -29,50 +29,51 @@ char *getEnvironmentValue(info_t *info, const char *name)
 }
 
 /**
- * setEnvironmentVariable - Initialize a new environment variable or modify an existing one.
+ * set_environment_variable - Initialize a new environment variable or modify an existing one.
  * @info: Structure containing potential arguments.
  * Return: 0 on success, 1 on failure.
  */
-int setEnvironmentVariable(info_t *info)
+int set_environment_variable(info_t *info)
 {
     if (info->argc != 3)
     {
-        _eputs("Incorrect number of arguments\n");
+        write(2, "Incorrect number of arguments\n", 31);
         return 1;
     }
-    
+
     return _setenv(info, info->argv[1], info->argv[2]) ? 0 : 1;
 }
 
 /**
- * unsetEnvironmentVariable - Remove an environment variable.
+ * unset_environment_variable - Remove an environment variable.
  * @info: Structure containing potential arguments.
  * Return: 0 on success, 1 on failure.
  */
-int unsetEnvironmentVariable(info_t *info)
+int unset_environment_variable(info_t *info)
 {
     if (info->argc == 1)
     {
-        _eputs("Too few arguments.\n");
+        write(2, "Too few arguments.\n", 19);
         return 1;
     }
-    
+
     for (int i = 1; i < info->argc; i++)
         _unsetenv(info, info->argv[i]);
-    
+
     return 0;
 }
 
 /**
- * populateEnvironmentList - Populate env linked list with environment variables.
+ * populate_environment_list - Populate env linked list with environment variables.
  * @info: Structure containing potential arguments.
  * Return: 0 on success, 1 on failure.
  */
-int populateEnvironmentList(info_t *info)
+int populate_environment_list(info_t *info)
 {
+    char **environ = get_environ();
     for (size_t i = 0; environ[i]; i++)
         add_node_end(&info->env, environ[i], 0);
-    
+
     return 0;
 }
 
